366aa8f22b6bb58329685b57eb70aa4c9141d429
curgitb <excurry93@gmail.com>
2026-03-01 22:21:04 +0800

    删除文件

--- 11.2 蜂鸣器播放音乐/Buzzer.h
+++
@@ -1,7 +0,0 @@
-#ifndef __BUZZER_H__
-#define	__BUZZER_H__
-
-void Buzzer_Delay500us();		//@11.0592MHz
-void Buzzer_Time(unsigned int xms);
-
-#endif
--- 11.2 蜂鸣器播放音乐/Timer0.h
+++
@@ -1,6 +0,0 @@
-#ifndef __TIMER0INIT_H__
-#define __TIMER0INIT_H__
-
-void Timer0Init(void);		//1000微秒@11.0592MHz
-
-#endif
